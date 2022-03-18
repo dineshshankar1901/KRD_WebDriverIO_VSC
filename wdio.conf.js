9d70a28b6eae5bb8b9a32e24da335dab70f80e85
Kandasamy Rajan <dinesh-shankar.kandasamy-rajan@capgemini.com>
2022-03-18 04:07:29 -0400

    updated

--- wdio.conf.js
+++ wdio.conf.js
@@ -3,6 +3,11 @@ exports.config = {
     // ====================
     // Runner Configuration
     // ====================
+    // browserstack Config
+    
+    //user: process.env.BROWSERSTACK_USERNAME,
+    //key: process.env.BROWSERSTACK_ACCESS_KEY,
+           
     //
     //
     // ==================
@@ -110,8 +115,8 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
+  
     services: ['chromedriver'],
-    
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: https://webdriver.io/docs/frameworks
